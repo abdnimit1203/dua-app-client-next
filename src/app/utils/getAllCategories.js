ed444712e3812689f755996b01146fe05157c11a
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-02-04 01:49:48 +0600

    Static site generated

--- src/app/utils/getAllCategories.js
+++ src/app/utils/getAllCategories.js
@@ -1,5 +1,5 @@
 
 export const getAllCategories = async()=>{
-    const res = await fetch("http://localhost:3003/api/category")
+    const res = await fetch("http://localhost:3003/api/category",{ cache: "force-cache"})
     return res.json()
 }
--- src/app/utils/getSubCategorybyCategory.js
+++ src/app/utils/getSubCategorybyCategory.js
@@ -1,6 +1,6 @@
 export const getSubcategorybyCategory = async (id) => {
   const res = await fetch(
-    `http://localhost:3003/api/sub_category?cat_id=${id}`
+    `http://localhost:3003/api/sub_category?cat_id=${id}`,{ cache: "force-cache"}
   );
 
   return res.json();
